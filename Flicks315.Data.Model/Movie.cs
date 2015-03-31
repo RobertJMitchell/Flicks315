5daadacfbbacbc51c0b06f25a09bc384ee6fc17d
RobertJMitchell <rjrocks32@gmail.com>
2015-03-31 10:14:39 -0500

    added foreignkey for data.model actor.cs

---
+++ Flicks315.Data.Model/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flicks315.Data.Model
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Rating { get; set; }
+        public string Description { get; set; }
+
+       
+    }
+
+
+}
